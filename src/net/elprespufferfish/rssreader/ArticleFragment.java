089dcd4c05d04e6365bb01571c3a2c8bf508faab
Michael Zampani <elprespufferfish@gmail.com>
2014-08-17 20:56:31 -0700

    Add Share button
    Fixes #7

--- src/net/elprespufferfish/rssreader/ArticleFragment.java
+++ src/net/elprespufferfish/rssreader/ArticleFragment.java
@@ -11,6 +11,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class ArticleFragment extends Fragment {
@@ -24,10 +25,11 @@ public class ArticleFragment extends Fragment {
         View view = inflater.inflate(R.layout.article_fragment, container, false);
         Bundle arguments = getArguments();
 
-        ((TextView) view.findViewById(R.id.title)).setText(arguments.getString(TITLE_KEY));
+        final String title = arguments.getString(TITLE_KEY);
+        ((TextView) view.findViewById(R.id.title)).setText(title);
 
-        TextView link = (TextView) view.findViewById(R.id.link);
         final String linkAddress = arguments.getString(LINK_KEY);
+        TextView link = (TextView) view.findViewById(R.id.link);
         link.setText(linkAddress);
         link.setClickable(true);
         link.setOnClickListener(new OnClickListener() {
@@ -38,10 +40,25 @@ public class ArticleFragment extends Fragment {
             }
         });
 
+        final String description = arguments.getString(DESCRIPTION_KEY);
         WebView webView = (WebView) view.findViewById(R.id.description);
         webView.setWebChromeClient(new WebChromeClient()); // HTML5 video requires a WebChromeClient
         enableJavaScript(webView);
-        webView.loadData(arguments.getString(DESCRIPTION_KEY), "text/html", null);
+        webView.loadData(description, "text/html", null);
+
+        ImageButton shareButton = (ImageButton) view.findViewById(R.id.share);
+        shareButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textToShare = title + "\n\n" + linkAddress;
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_SUBJECT, title);
+                intent.putExtra(Intent.EXTRA_TEXT, textToShare);
+                intent.setType("text/plain");
+                startActivity(Intent.createChooser(intent, getResources().getText(R.string.share_title)));
+            }
+        });
 
         return view;
     }
