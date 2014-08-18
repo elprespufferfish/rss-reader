3047e4ace9a36f6cf2ac838891e0a6882552e129
Michael Zampani <elprespufferfish@gmail.com>
2014-08-17 18:27:30 -0700

    Use OpenGraph image as description
    Fixes #4

--- src/net/elprespufferfish/rssreader/Article.java
+++ src/net/elprespufferfish/rssreader/Article.java
@@ -5,8 +5,14 @@ import java.io.IOException;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
+
+import android.util.Log;
 
 /**
  * Represents a single item to be read
@@ -25,6 +31,7 @@ public class Article implements Comparable<Article> {
     public static Article fromXml(XmlPullParser xmlPullParser) throws XmlPullParserException, IOException {
         Builder builder = new Builder();
 
+        boolean hasOpenGraphContent = false;
         String text = null;
         int eventType = xmlPullParser.getEventType();
         while (eventType != XmlPullParser.END_DOCUMENT) {
@@ -45,10 +52,18 @@ public class Article implements Comparable<Article> {
                     builder.setTitle(text);
                 } else if ("".equals(namespace) && "link".equals(nodeName)) {
                     builder.setLink(text);
+                    String openGraphContent = getOpenGraphContent(text);
+                    if (openGraphContent != null) {
+                        hasOpenGraphContent = true;
+                        builder.setDescription(openGraphContent);
+                    }
                 } else if ("".equals(namespace) && "pubDate".equals(nodeName)) {
                     builder.setPublicationDate(RFC822_FORMATTER.parseDateTime(text));
                 } else if ("".equals(namespace) && "description".equals(nodeName)) {
-                    builder.setDescription(text);
+                    if (!hasOpenGraphContent) {
+                        // fall back to plain description if nothing better is available
+                        builder.setDescription(text);
+                    }
                 } else if ("".equals(namespace) && "item".equals(nodeName)) {
                     // return immediately to ensure we do not advance the
                     // pull parser too far
@@ -64,6 +79,29 @@ public class Article implements Comparable<Article> {
         }
 
         throw new IllegalStateException("Reached end of document without closing </item>");
+    }
+
+    /**
+     * @return opengraph content at provided content if available
+     */
+    private static String getOpenGraphContent(String articleAddress) {
+        try {
+            Document document = Jsoup.connect(articleAddress).get();
+            String imageAddress = getOpenGraphContent(document, "image");
+            return "<html><body><img src=\"" + imageAddress + "\" width=\"100%\"/></body></html>";
+        } catch (Exception e) {
+            Log.e("rss-reader", "Could not crawl for opengraph content", e);
+        }
+        return null;
+    }
+
+    private static String getOpenGraphContent(Document document, String type) {
+        Elements elements = document.select("meta[property=og:" + type);
+        if (elements.size() == 0) {
+            return null;
+        }
+        Element element = elements.first();
+        return element.attr("content");
     }
 
     private static class Builder {
