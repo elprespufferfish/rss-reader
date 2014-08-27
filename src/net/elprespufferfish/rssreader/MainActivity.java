67b6e7d76bca5d1e439dc040ec51ec085cf36368
Michael Zampani <elprespufferfish@gmail.com>
2014-08-26 21:42:42 -0700

    Reuse RefreshTask

--- src/net/elprespufferfish/rssreader/MainActivity.java
+++ src/net/elprespufferfish/rssreader/MainActivity.java
@@ -2,6 +2,7 @@ package net.elprespufferfish.rssreader;
 
 import static android.widget.Toast.LENGTH_SHORT;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -21,56 +22,58 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        final ProgressDialog dialog = ProgressDialog.show(this, "", getString(R.string.loading_articles), true);
-
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... params) {
-                DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
-                SQLiteDatabase database = databaseHelper.getWritableDatabase();
-                try {
-                    Feeds.getInstance().refresh(database);
-                } finally {
-                    database.close();
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void v) {
-                ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-                viewPager.setAdapter(new ArticlePagerAdapter(MainActivity.this.getSupportFragmentManager(), MainActivity.this));
-
-                dialog.dismiss();
-            }
-
-        }.execute();
+        new RefreshTask(this).execute();
 
         ListView drawer = (ListView) findViewById(R.id.left_drawer);
         drawer.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_item, new String[] { "Refresh" }));
         drawer.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                new AsyncTask<Void, Void, Boolean>() {
-                    @Override
-                    protected Boolean doInBackground(Void... params) {
-                        DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
-                        SQLiteDatabase database = databaseHelper.getWritableDatabase();
-                        return Feeds.getInstance().refresh(database);
-                    }
-
-                    @Override
-                    protected void onPostExecute(Boolean wasRefreshStarted) {
-                        if (!wasRefreshStarted) {
-                            Toast.makeText(
-                                    MainActivity.this,
-                                    MainActivity.this.getString(R.string.refresh_already_started),
-                                    LENGTH_SHORT)
-                                    .show();
-                        }
-                    }
-                }.execute();
+                new RefreshTask(MainActivity.this).execute();
             }
         });
+    }
+
+    private class RefreshTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final ProgressDialog progressDialog;
+
+        public RefreshTask(Context context) {
+            this.progressDialog = new ProgressDialog(context);
+            progressDialog.setMessage(context.getString(R.string.loading_articles));
+            progressDialog.setIndeterminate(true);
+            progressDialog.setCancelable(false);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
+            SQLiteDatabase database = databaseHelper.getWritableDatabase();
+            try {
+                return Feeds.getInstance().refresh(database);
+            } finally {
+                database.close();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean wasRefreshStarted) {
+            progressDialog.dismiss();
+            if (!wasRefreshStarted) {
+                Toast.makeText(
+                        MainActivity.this,
+                        MainActivity.this.getString(R.string.refresh_already_started),
+                        LENGTH_SHORT)
+                        .show();
+            } else {
+                ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+                viewPager.setAdapter(new ArticlePagerAdapter(MainActivity.this.getSupportFragmentManager(), MainActivity.this));
+            }
+        }
     }
 }
