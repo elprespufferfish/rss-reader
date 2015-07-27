5d32b9819f43e06ff283e9a65c8f954b46923096
Michael Zampani <elprespufferfish@gmail.com>
2015-07-26 18:44:37 -0700

    #8: Enable StrictMode

--- src/main/java/net/elprespufferfish/rssreader/RssReaderApplication.java
+++ src/main/java/net/elprespufferfish/rssreader/RssReaderApplication.java
@@ -1,6 +1,7 @@
 package net.elprespufferfish.rssreader;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 public class RssReaderApplication extends Application {
 
@@ -9,5 +10,19 @@ public class RssReaderApplication extends Application {
         super.onCreate();
 
         Feeds.initialize();
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+
+        }
     }
 }
