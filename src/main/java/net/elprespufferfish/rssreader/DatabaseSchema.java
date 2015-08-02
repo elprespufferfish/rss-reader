a6a0ff97fb908e8c32387a9af89fe2a87fd00954
Michael Zampani <elprespufferfish@gmail.com>
2015-08-02 12:23:39 -0700

    Remove redundant modifiers for interface fields

--- src/main/java/net/elprespufferfish/rssreader/DatabaseSchema.java
+++ src/main/java/net/elprespufferfish/rssreader/DatabaseSchema.java
@@ -4,21 +4,21 @@ import android.provider.BaseColumns;
 
 public class DatabaseSchema {
 
-    public static interface FeedTable extends BaseColumns {
-        public static final String TABLE_NAME = "feeds";
-        public static final String FEED_NAME = "feed_name";
-        public static final String FEED_URL = "feed_url";
+    public interface FeedTable extends BaseColumns {
+        String TABLE_NAME = "feeds";
+        String FEED_NAME = "feed_name";
+        String FEED_URL = "feed_url";
     }
 
-    public static interface ArticleTable extends BaseColumns {
-        public static final String TABLE_NAME = "articles";
-        public static final String ARTICLE_FEED = "article_feed";
-        public static final String ARTICLE_NAME = "article_name";
-        public static final String ARTICLE_URL = "article_url";
-        public static final String ARTICLE_PUBLICATION_DATE = "article_pubdate";
-        public static final String ARTICLE_DESCRIPTION = "article_description";
-        public static final String ARTICLE_GUID = "article_guid";
-        public static final String ARTICLE_IS_READ = "article_is_read";
+    public interface ArticleTable extends BaseColumns {
+        String TABLE_NAME = "articles";
+        String ARTICLE_FEED = "article_feed";
+        String ARTICLE_NAME = "article_name";
+        String ARTICLE_URL = "article_url";
+        String ARTICLE_PUBLICATION_DATE = "article_pubdate";
+        String ARTICLE_DESCRIPTION = "article_description";
+        String ARTICLE_GUID = "article_guid";
+        String ARTICLE_IS_READ = "article_is_read";
     }
 
     private DatabaseSchema() {
