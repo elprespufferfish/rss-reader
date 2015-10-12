123d67013c4e89ab2bccd66ef5ff0b5161e8974b
Michael Zampani <elprespufferfish@gmail.com>
2015-10-11 21:34:31 -0700

    Compute count once since data shold not change without reloading the pager

--- src/main/java/net/elprespufferfish/rssreader/ArticlePagerAdapter.java
+++ src/main/java/net/elprespufferfish/rssreader/ArticlePagerAdapter.java
@@ -22,26 +22,17 @@ public class ArticlePagerAdapter extends FragmentStatePagerAdapter {
      * NOTE: May be null to represent 'all feeds'
      */
     private final String feedUrl;
+    private final int count;
 
     public ArticlePagerAdapter(FragmentManager fm, Context context, String feedUrl) {
         super(fm);
         DatabaseHelper databaseHelper = new DatabaseHelper(context);
         this.database = databaseHelper.getReadableDatabase();
         this.feedUrl = feedUrl;
+        this.count = computeCount();
     }
 
-    @Override
-    public Fragment getItem(int i) {
-        Fragment fragment = new ArticleFragment();
-        Bundle bundle = new Bundle();
-        bundle.putString(ArticleFragment.ARTICLE_FEED_URL_KEY, feedUrl);
-        bundle.putInt(ArticleFragment.ARTICLE_INDEX_KEY, i);
-        fragment.setArguments(bundle);
-        return fragment;
-    }
-
-    @Override
-    public int getCount() {
+    private int computeCount() {
         String[] selectionArgs = new String[0];
         String query = "SELECT COUNT(*)" +
                 "FROM " + ArticleTable.TABLE_NAME + " ";
@@ -59,6 +50,21 @@ public class ArticlePagerAdapter extends FragmentStatePagerAdapter {
         } finally {
             articleCountCursor.close();
         }
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        Fragment fragment = new ArticleFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(ArticleFragment.ARTICLE_FEED_URL_KEY, feedUrl);
+        bundle.putInt(ArticleFragment.ARTICLE_INDEX_KEY, i);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return count;
     }
 
     /**
